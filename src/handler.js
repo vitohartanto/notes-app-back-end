9eaaf945bc12191dd6d447c85e2ea589bb05dd2c
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-03 17:05:15 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,8 @@
+const { nanoid } = require('nanoid');
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+  const id = nanoid(16);
+};
+
+module.exports = { addNoteHandler };
