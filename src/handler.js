1edc1b24578066f7a3c419cf926491d4bee84a02
Vito <vitohartanto@mail.ugm.ac.id>
2024-06-03 17:07:28 +0700

    feat: handlerjs

--- src/handler.js
+++ src/handler.js
@@ -3,6 +3,8 @@ const { nanoid } = require('nanoid');
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
   const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
 };
 
 module.exports = { addNoteHandler };
