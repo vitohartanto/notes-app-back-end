dc69d90d4c62d4a0da648bec913f77a5ae358807
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-30 21:08:23 +0700

    deleteAuthenticationHandler done

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -30,4 +30,35 @@ class AuthenticationsHandler {
     response.code(201);
     return response;
   };
+
+  putAuthenticationHandler = async (request, h) => {
+    this._validator.validatePutAuthenticationPayload(request.payload);
+
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+    const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+    return {
+      status: 'success',
+      message: 'Access Token berhasil diperbarui',
+      data: {
+        accessToken,
+      },
+    };
+  };
+
+  deleteAuthenticationHandler = async (request, h) => {
+    this._validator.validateDeleteAuthenticationPayload(request.payload);
+
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+
+    await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+    return {
+      status: 'success',
+      message: 'Refresh token berhasil dihapus',
+    };
+  };
 }
