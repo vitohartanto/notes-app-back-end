9b9033b58172c7fb198a4553d4ed549f72a19db7
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-30 08:27:44 +0700

    UsersService.js done

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UsersService {
   constructor() {
@@ -11,6 +13,19 @@ class UsersService {
     await this.verifyNewUsername(username);
 
     const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+    return result.rows[0].id;
   };
 
   verifyNewUsername = async (username) => {
@@ -26,6 +41,21 @@ class UsersService {
         'Gagal menambahkan user. Username sudah digunakan'
       );
     }
+  };
+
+  getUserById = async (userId) => {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
   };
 }
 
