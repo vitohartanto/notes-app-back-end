034337eba130b74311630f07ac66976b1b9161eb
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-30 08:46:28 +0700

    handler.js for users done

---
+++ src/api/users/handler.js
@@ -0,0 +1,46 @@
+class UsersHandlers {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  postUserHandler = async (request, h) => {
+    this._validator.validateUserPayload(request.payload);
+
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addUser({
+      username,
+      password,
+      fullname,
+    });
+
+    const response = h.response({
+      status: 'success',
+      message: 'User berhasil ditambahkan',
+      data: {
+        userId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  };
+
+  getUserByIdHandler = async (request, h) => {
+    const { id } = request.params;
+    const user = await this._service.getUserById(id);
+
+    return {
+      status: 'success',
+      data: {
+        user,
+      },
+    };
+  };
+}
+
+module.exports = UsersHandlers;
