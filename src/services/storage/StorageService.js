47d9a1baac14ab4e9851614fd151dc36c215492b
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-07 20:13:19 +0700

    Storage Service done

--- src/services/storage/StorageService.js
+++ src/services/storage/StorageService.js
@@ -8,5 +8,18 @@ class StorageService {
     }
   }
 
-  writeFile(file, meta) {}
+  writeFile(file, meta) {
+    const filename = +new Date() + meta.filename;
+    const path = `${this._folder}/${filename}`;
+
+    const fileStream = fs.createWriteStream(path);
+
+    return new Promise((resolve, reject) => {
+      fileStream.on('error', (error) => reject(error));
+      file.pipe(fileStream);
+      file.on('end', () => resolve(filename));
+    });
+  }
 }
+
+module.exports = StorageService;
